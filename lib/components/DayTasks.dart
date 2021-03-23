18173e30a7672efffbcbb32557149180698ff45a
Timothy Karani <kartimothy@gmail.com>
2021-03-23 23:31:32 +0300

    Basic day tasks added

--- lib/components/DayTasks.dart
+++ lib/components/DayTasks.dart
@@ -9,9 +9,33 @@ class DayTasks extends StatelessWidget {
           left: 25,
         ),
         child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            color: Colors.grey[900],
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 20,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.grey[900],
+                  ),
+                  height: 120,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 20,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.grey[900],
+                  ),
+                  height: 120,
+                ),
+              ),
+            ],
           ),
         ),
       ),
