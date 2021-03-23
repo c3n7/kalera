26caedb83c8fa4c7302e3da3a258f93bfab92a87
Timothy Karani <kartimothy@gmail.com>
2021-03-24 00:05:10 +0300

    Daily task layout done

--- lib/components/DayTask.dart
+++ lib/components/DayTask.dart
@@ -13,6 +13,38 @@ class DayTask extends StatelessWidget {
           color: Colors.grey[900],
         ),
         height: 120,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.grey[700],
+                ),
+                height: 100,
+                width: 120,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: 10,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.grey[700],
+                  ),
+                  height: 100,
+                  width: 120,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
