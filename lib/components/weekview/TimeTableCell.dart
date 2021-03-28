f5933eac70c7c623b2f8bb578b2d1b32d8007bb9
Timothy Karani <kartimothy@gmail.com>
2021-03-28 13:00:19 +0300

    Tweaked time sizing

---
+++ lib/components/weekview/TimeTableCell.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TimeTableCell extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TableCell(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 3, vertical: 0),
+        child: Container(
+          height: 20,
+          width: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(2)),
+            color: Colors.grey[800],
+          ),
+        ),
+      ),
+    );
+  }
+}
